9200d0e17f828251598c7c7d399f2fdb28e9be06
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2019-02-16 14:48:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -59,40 +59,18 @@ if(message.content.startsWith(">appel") ){
                ">sms_help **message**\n>sms (mention personne) **message**")
      message.channel.send({embed});
 }
-if(message.content == ">Numbor start"){
-    if(party_launch == true){
-        message.channel.send(`la partie est déjà lancé ${message.author} ^^`)
-    }else{
 
-    if (party_launch == false){
-        message.channel.send(":arrow_forward: cette manche sera entre 0 est 5000 :arrow_forward: ")
-        
-        number_random = Math.floor(Math.random() * (5000 - 0) + 0)
-        console.log(number_random);
-party_launch = true;
-}
-}
-if(party_launch && message.content !=null){
-    if(Number.isInteger(parseInt(message.content))){
-        if(message.content > number_random){
-            message.channel.send(":arrow_down: plus petit, stp :arrow_down: ")
-        }
-        else if(message.content < number_random){
-            message.channel.send(":arrow_up: plus grand, svp :arrow_up: ")
-        }
-        else{
-                message.channel.send(`:ballot_box_with_check: ${message.author} a gagner cette manche, t'es trop fort <3 :ballot_box_with_check:`);
-            party_launch = false
-        }
-    }
-}
-if(message.content == ">numbor stop"){
-    if(party_launch == true){
-            message.channel.send(`:stop_button: ${message.author}, la partie est stoppé ^^ :stop_button: `)
-        party_launch = false;
-}else{
- message.channel.send("-_- l'application n'est pas lancé (gogole)")
-}
+if (message.content.startsWith(prefix + "journal")){
+    var embed = new Discord.RichEmbed()
+    .setTitle("Iphone X Demon")
+    .addField("***Journal des Démons du mois de Février***", "ㅤㅤ")
+    .addField("Union Upsilon : Extermination par les démons ?", "La semaine dernière , L'Union Upsilon à du faire face au 3 chefs démoniaque !\nLeur chef , Guten , nous explique leur défaite :\n*Nous étions environ 50 000 face à eux ! Notre Union étant l'une des plus puissante , pensions gagner facilement quand les 3 grands chefs démoniaque sont apparus et ont exterminés les*")
+    .addField("ㅤㅤ",
+    "*3 quarts de mes hommes ... Surtout que les chefs démoniaques n'étaient normalement que rang D ! Je ne sais pas ce qui leur est arrivé , mais ils sont devenus beaucoup plus fort qu'avant , et*")
+    .addField("ㅤㅤ",
+    "*c'est d'ailleurs pour cette raison que ces 3 là sont passés en rang B ! Même leur pion était plus fort et intelligent qu'avant , ils ont changés quelques chose mais quoi ? Nous envoyons une Union plus\nforte enquêter sur le sujet !*\nAlors est-ce que les démons ont pris la relève ? Va t'on reperdre notre territoire et se faire exterminer une bonne fois pour toute ? A suivre ...")
+    .setColor(0xD4FE00)
+    message.channel.send(embed)
 }
 }
 )
