63934b33c8125faea4c52e44907d3c445e902bbc
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2019-02-16 13:23:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const Discord = require('discord.js');
      .setColor(0xD4FE00)
      .setDescription("----------")
      .addField("               X                   X                    X                  ",
-               "             >appel              >livre                >help               ")
+               "             >appel              >livre                >journal               ")
  
      message.channel.send({embed});
 }
@@ -56,6 +56,17 @@ if(message.content.startsWith(">appel") ){
      .addField("Partie SMS",
                ">sms_help **message**\n>sms (mention personne) **message**")
      message.channel.send({embed});
+}
+if(message.content.startsWith(">journal") ){
+    const embed = new Discord.RichEmbed()
+    .setTitle("Iphone X Demon")
+    .setColor(0xD4FE00)
+    .setDescription("----------")
+    .setDescription("***Journal des Démons du mois de Février***")
+    .addField("Union Upsilon : Extermination par les démons ?",
+              "La semaine dernière , L'Union Upsilon à du faire face au 3 chefs démoniaque !\nLeur chef , Guten , nous explique leur défaite :\n*Nous étions environ 50 000 face à eux ! Notre Union étant l'une des plus puissante , pensions gagner facilement quand les 3 grands chefs démoniaque sont apparus et ont exterminés les\n3 quarts de mes hommes ... Surtout que les chefs démoniaques n'étaient normalement que rang D ! Je ne sais pas ce qui leur est arrivé , mais ils sont devenus beaucoup plus fort qu'avant , et*")
+    .setDescription("*c'est d'ailleurs pour cette raison que ces 3 là sont passés en rang B ! Même leur pion était plus fort et intelligent qu'avant , ils ont changés quelques chose mais quoi ? Nous envoyons une Union plus # forte enquêter sur le sujet !*")
+    .setDescription("Alors est-ce que les démons ont pris la relève ? Va t'on reperdre notre territoire et se faire exterminer une bonne fois pour toute ? A suivre ...")
 }
 }
 )
